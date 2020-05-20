a823431f8407144a1015a650d6b8076ad0d84744
pavakta <pavakta@myseneca.ca>
2020-05-20 00:46:21 -0400

    add SequenceMatcher

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import json
+from difflib import SequenceMatcher
 
 data = json.load(open('data.json'))
+
+def match(word):
+    matched_word = []
+    for w in data.keys():
+        if SequenceMatcher(None, w, word).ratio() > 0.8:
+            matched_word.append(w)
+    return matched_word
 
 def lookup(word):
     word = word.lower()
     if word in data.keys():
         return data[word]
     else:
-        return "The word doesn't exist! Please check the spelling."
+        matched_words = match(word)
+        if len(matched_words) > 0:
+            print('Did you mean any of these words?')
+            return matched_words
+        else:
+            return "The word doesn't exist! Please check the spelling."
 
 user_input = input('Enter a word to lookup the dictionary: ')
 
